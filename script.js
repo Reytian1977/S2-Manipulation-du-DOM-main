da95c3e2f779aeeb3b2fff823c7a259bec9870ec
Sebastian Cabrera <sebascabrera102@gmail.com>
2023-01-18 16:09:43 -0500

    premier test

--- script.js
+++ script.js
@@ -6,3 +6,39 @@ const panier = {
 
 // Pour afficher le nombre de pommes dans la facture détaillée
 document.querySelector("#qtePommes").textContent = panier.pommes;
+
+
+function Ajouter(fruit) {
+    let Prix;
+    let Poids;
+    let id;
+
+    if(fruit === 1)
+    {
+        Prix = 13.50;
+        Poids = 10;
+        id = "poidsPommes";
+    }
+    if(fruit === 2)
+    {
+        Prix = 20.00;
+        Poids = 12;
+        id = "poidsPoires";
+    }
+    if(fruit === 3)
+    {
+        Prix = 22.00;
+        Poids = 15;
+        id = "poidsPrunes";
+    }
+
+    // Mettre à jour la Quantité
+    let Qte = document.getElementById("qtePommes").innerText;
+    let NewQte = +Qte + +Qte;
+    document.getElementById("qtePommes").innerText = NewQte;
+
+    // Mettre à jour le Poids
+    Poids = document.getElementById(id).innerText;
+    let NewPoids = +Poids + +Poids;
+    document.getElementById(id).innerText = NewPoids;
+}
